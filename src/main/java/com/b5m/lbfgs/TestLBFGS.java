8cbabdee7209b4c522c0a6bf3ab1de7bace51f8a
Alex <alex.wang@b5m.com>
2014-03-06 15:33:33 +0800

    Signed-off-by: Alex <alex.wang@b5m.com>

--- src/main/java/com/b5m/lbfgs/TestLBFGS.java
+++ src/main/java/com/b5m/lbfgs/TestLBFGS.java
@@ -1,4 +1,19 @@
 package com.b5m.lbfgs;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.regex.Pattern;
+
+import org.apache.mahout.math.IndexException;
+import org.apache.mahout.math.Matrix;
+import org.apache.mahout.math.SparseRowMatrix;
+
+import com.b5m.admm.AdmmMapperContext;
 /* copyright 2002 by Robert Dodier
 *
 * This program is free software; you can redistribute it and/or modify
@@ -9,66 +24,77 @@ package com.b5m.lbfgs;
 * (2) The Apache license, version 2.
 */
 
+import edu.stanford.nlp.optimization.DiffFunction;
+import edu.stanford.nlp.optimization.QNMinimizer;
+
 
 public class TestLBFGS
 {
-	static int ndim = 2000 , msave = 7 ;
-	static int nwork = ndim * ( 2 * msave + 1 ) + 2 * msave ;
+	 private static final Pattern TAB_PATTERN = Pattern.compile("\t");
+	    private static final Pattern NEWLINE_PATTERN = Pattern.compile("\\n");
+	    private static final Pattern SPACE_PATTERN = Pattern.compile("\\s+");
+	    private static final Pattern COLON_PATTERN = Pattern.compile(":");
+		public static Matrix createMatrixFromDataString(
+				String dataString, int numFeatures, Set<Integer> columnsToExclude,
+				boolean addIntercept) throws IndexException {
+			String[] rows = NEWLINE_PATTERN.split(dataString);
+			int numRows = rows.length;
 
-	public static void main( String args[] )
-	{
-		double x [ ] , g [ ] , diag [ ] , w [ ];
-		x = new double [ ndim ];
-		g = new double [ ndim ];
-		diag = new double [ ndim ];
-		w = new double [ nwork ];
+			Matrix data = new SparseRowMatrix(numRows, numFeatures + 1);
 
-		double f, eps, xtol, gtol, t1, t2, stpmin, stpmax;
-		int iprint [ ] , iflag[] = new int[1], icall, n, m, mp, lp, j;
-		iprint = new int [ 2 ];
-		boolean diagco;
+			for (int i = 0; i < numRows; i++) {
+				String[] elements = TAB_PATTERN.split(rows[i]);
+				if (addIntercept) {
+					data.set(i, 0, 1.0);
+				}
+				for (int j = 0; j < elements.length - 1; ++j) {
+					String[] element = COLON_PATTERN.split(elements[j]);
+					Integer featureId = Integer.parseInt(element[0]);
+					if (columnsToExclude.contains(featureId)) {
+						continue;
+					}
+					Double feature = Double.parseDouble(element[1]);
+					try {
+						data.set(i, featureId, feature);
+					} catch (IndexException e) {
+						throw e;
+					}
+				}
+				if (elements.length >= 2) {
+					data.set(i, numFeatures,
+							Double.parseDouble(elements[elements.length - 1]));
+				}
+			}//System.out.println(data.toString());
+			return data;
+		}
+		String loadData(String path) throws IOException{
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+			String data ;
+			StringBuffer data2 = new StringBuffer();
+			int line = 0;
+			while (  (data = br.readLine()) != null) {
+				data2.append("\n").append(data);
+			}
+			
+			return data2.substring(1).toString();
+		}
+		public static void main(String[] args) throws IOException {
+			// TODO Auto-generated method stub
+			
+			TestLBFGS  tbfgs= new TestLBFGS();
+			String value=tbfgs.loadData("D://Download/sparse-logreg_features");
+			Set<Integer> columnsToExclude = new HashSet();
+	        Matrix inputSplitData = tbfgs.createMatrixFromDataString(value, 4, columnsToExclude, true);
+	        AdmmMapperContext context = new AdmmMapperContext("0", inputSplitData);
+	        LogisticL2DiffFunction myFunction =
+	                new LogisticL2DiffFunction(context.getA(),
+	                        context.getB(),
+	                        context.getRho(),
+	                        context.getUInitial(),
+	                        context.getZInitial());
+	        QNMinimizer lbfgs = new QNMinimizer();
+	        double[] optimum = lbfgs.minimize((DiffFunction)myFunction, 1e-10, context.getXInitial());
+	        System.out.println(optimum);
+		}
 		
-		n=100;
-		m=5;
-		iprint [ 1 -1] = 3;
-		iprint [ 2 -1] = 3;
-		diagco= false;
-		eps= 1.0e-5;
-		xtol= 1.0e-16;
-		icall=0;
-		iflag[0]=0;
-
-		for ( j = 1 ; j <= n ; j += 2 )
-		{
-			x [ j -1] = - 1.2e0;
-			x [ j + 1 -1] = 1.e0;
-		}
-
-		do
-		{
-			f= 0;
-			System.out.println("\nITER:ICALL TO CHANGE");
-			for ( j = 1 ; j <= n ; j += 2 )
-			{
-				t1 = 1.e0 - x [ j -1];
-				t2 = 1.e1 * ( x [ j + 1 -1] - x [ j -1] * x[j-1] );
-				g [ j + 1 -1] = 2.e1 * t2;
-				g [ j -1] = - 2.e0 * ( x [ j -1] * g [ j + 1 -1] + t1 );
-				f= f+t1*t1+t2*t2;
-			}
-
-			try
-			{
-				LBFGS.lbfgs ( n , m , x , f , g , diagco , diag , iprint , eps , xtol , iflag );
-			}
-			catch (LBFGS.ExceptionWithIflag e)
-			{
-				System.err.println( "Sdrive: lbfgs failed.\n"+e );
-				return;
-			}
-			System.out.println("\nITER:ICALL"+icall);
-			icall += 1;
-		}
-		while ( iflag[0] != 0 && icall <= 200 );
-	}
 }
